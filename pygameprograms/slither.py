8e91e072c1e1c0b474d386efaa0a1601337cf36d
Rahul kumar <lit2015023@iiita.ac.in>
2017-03-16 21:26:06 +0530

    moving rectangles

--- pygameprograms/slither.py
+++ pygameprograms/slither.py
@@ -7,13 +7,21 @@ black = (0,0,0)
 white = (255,255,255)
 red = (255,0,0)
 green = (0,255,0)
+lead_x = 400
+lead_y = 200
 gameExit = False
 while not gameExit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameExit = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                lead_x = lead_x - 25
+            if event.key == pygame.K_RIGHT:
+                lead_x = lead_x + 25
+        
     gameDisplay.fill(blue)
-    pygame.draw.rect(gameDisplay,black,[400,200,25,25])
+    pygame.draw.rect(gameDisplay,black,[lead_x,lead_y,25,25])
     pygame.display.update()
 pygame.quit()
 
