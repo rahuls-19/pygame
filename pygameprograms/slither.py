d48acdcbb9424255fa815efb6f68c84b66b9eea0
Rahul kumar <lit2015023@iiita.ac.in>
2017-03-16 20:09:27 +0530

    color filling

--- pygameprograms/slither.py
+++ pygameprograms/slither.py
@@ -1,10 +1,19 @@
 import pygame
 pygame.init()
 gameDisplay = pygame.display.set_mode((800,400))
+pygame.display.set_caption("Slither")
+blue = (0,0,255)
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+green = (0,255,0)
 gameExit = False
 while not gameExit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameExit = True
+    gameDisplay.fill(blue)
+    
+    pygame.display.update()
 pygame.quit()
 
